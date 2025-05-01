fb2139e6fc31b79e54645306d24817b55864b665
Md Akib Zabed Khan <38156511+akibzabedkhan@users.noreply.github.com>
2025-05-01 15:27:54 -0400

    Add files via upload

---
+++ GGCR/config_v28_v2_oser.py
@@ -0,0 +1,30 @@
+class Config(object):
+    def __init__(self):
+        self.TRAININGSET_DIR = '/a/bear.cs.fiu.edu./disk/bear-b/users/mkhan149/Downloads/Experiments/Others/CDREAM_LGCN/train_sample_all_v2.json'
+        self.VALIDATIONSET_DIR = '/a/bear.cs.fiu.edu./disk/bear-b/users/mkhan149/Downloads/Experiments/Others/CDREAM_LGCN/valid_sample_without_target_v2.json'
+        self.TESTSET_DIR = '/a/bear.cs.fiu.edu./disk/bear-b/users/mkhan149/Downloads/Experiments/Others/CDREAM_LGCN/target_set_v2.json'
+        self.NEG_SAMPLES = '/a/bear.cs.fiu.edu./disk/bear-b/users/mkhan149/Downloads/Experiments/Others/CDREAM_LGCN/neg_sample_v25.pickle'
+        self.MODEL_DIR = '/a/bear.cs.fiu.edu./disk/bear-b/users/mkhan149/Downloads/Experiments/Others/CDREAM_LGCN/runs_v20/'
+        self.cuda = False
+        self.clip = 10
+        #self.epochs = 200
+        self.epochs = 10 #25
+        #self.batch_size = 256
+        self.batch_size = 32
+        #self.seq_len = 20
+        self.seq_len = 20
+        self.learning_rate = 0.01  # Initial Learning Rate
+        self.log_interval = 1  # num of batches between two logging
+        self.basket_pool_type = 'avg'  # ['avg', 'max']
+        self.rnn_type = 'GRU'  # ['RNN_TANH', 'RNN_RELU', 'LSTM', 'GRU']
+        self.rnn_layer_num = 2 # 1 2 3
+        self.dropout = 0.3 #0.4 0.5
+        #self.num_product = 26991+1 
+        self.num_product = 618
+        #self.num_product = 617
+        #self.num_product = 601
+        #self.embedding_dim = 32  # Embedding Layer
+        self.embedding_dim = 128 # 8 16 32 
+        #self.neg_num = 500  
+        self.neg_num = 600
+        self.top_k = 10  # Top K 
